2ac9cfb94dbc27ef83757bfdf28917e8fd3372df
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-02-21 10:40:05 -0800

    MVP is completed

--- api/projects-model.js
+++ api/projects-model.js
@@ -1,9 +1,46 @@
 const db = require('../data/db-config');
 
 module.exports = {
-    projects
+    get,
+    addProject,
+    resources,
+    addResource,
+    tasks,
+    addTask,
+    tasksForProject
 }
 
-function projects() {
+function get() {
     return db('projects');
+}
+
+function addProject(project) {
+    console.log(project)
+    return db('projects')
+        .insert(project)
+}
+
+function resources() {
+    return db('resources');
+}
+
+function addResource(resource) {
+    return db('resources')
+        .insert(resource);
+}
+
+function tasks() {
+    return db('tasks');
+}
+
+function addTask(task) {
+    return db('tasks')
+        .insert(task);
+}
+
+function tasksForProject(id) {
+    return db('tasks as t')
+        .select('p.name as ProjectName','p.description as ProjectDescription','t.description as TaskDescription', 't.notes as TaskNotes', 't.completed as TaskCompleted')
+        .join('projects as p','t.project_id','p.id')
+        .where({ project_id: id })
 }
--- api/projects-router.js
+++ api/projects-router.js
@@ -5,8 +5,74 @@ const Projects = require('./projects-model');
 const router = express.Router();
 
 router.get('/', (req, res) => { 
-    res.status(200).json({ message: "made it to /api/projects !!!" });
+    Projects.get()
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "could not retrieve projects" });
+        })
 });
+
+router.post('/', (req, res) => {
+    Projects.addProject(req.body)
+        .then(project => {
+            res.status(201).json(project);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Could not add project" });
+        })
+})
+
+router.get('/resources', (req, res) => {
+    Projects.resources()
+        .then(resources => {
+            res.status(200).json(resources);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "could not get resources" });
+        })
+})
+
+router.post('/resources', (req, res) => {
+    Projects.addResource(req.body)
+        .then(resource => {
+            res.status(201).json(resource);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Could not add resource" });
+        })
+})
+
+router.get('/tasks', (req, res) => {
+    Projects.tasks()
+        .then(tasks => {
+            res.status(200).json(tasks);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Could not get tasks" });
+        })
+})
+
+router.post('/tasks', (req, res) => {
+    Projects.addTask(req.body)
+        .then(task => {
+            res.status(201).json(task);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Could not add task" });
+        })
+})
+
+router.get('/:id/tasks', (req, res) => {
+    Projects.tasksForProject(req.params.id)
+        .then(tasks => {
+            res.status(200).json(tasks);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Could not get tasks for that project" });
+        })
+})
 
 
 
